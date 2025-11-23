936b25f8e9db1126ff140892794db2b5dcb55f5f
MathPrm <primotmathilde@gmail.com>
2025-11-23 22:26:31 +0100

    feat(characters): define interface for characters classes

---
+++ app/classes/Personnage.py
@@ -0,0 +1,32 @@
+# interface that defines a contract for characters classes
+
+from abc import ABC, abstractmethod
+
+class Personnage(ABC):
+    
+    @abstractmethod
+    def fighting_move(self, opponent: str, attack_strength: int) -> None:
+        '''
+        This function allow the character to use a fighting move.
+        Args:
+            - opponent (str): name of the opponent against whom the player is attacking.
+            - attack_strength (int): strength of the attack.
+        '''
+        ...
+        
+    @abstractmethod
+    def dodge() -> None:
+        '''
+        This function allow the character to dodge an attack.
+        '''
+        ...
+        
+    @abstractmethod
+    def damages_taken(self, opponent: str, attack_strength: int) -> None:
+        '''
+        This function calculate the damages taken by the character.
+        Args:
+            - opponent (str): name of the opponent against whom the player is attacking.
+            - attack_strength (int): strength of the attack.
+        '''
+        ...
